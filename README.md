c7970c07fae9d5a418f2245dbb455ac19790ac4e
wpumacay <wpumacay@gmail.com>
2020-01-16 13:00:12 -0500

    [Maintenance]: updated dependencies repo-urls
    Changes:
    
    * Changed setup_dependencies script repo-urls to grab the
      appropriate versions|branches of the required dependencies.
    * Updated build rules to handle changes to repo-urls and repo-names.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tysocDart
-A set of C++ wrappers of DART for a locomotion framework.
+**Status**: Heavy development (you might run into crashes as the library is being developed, sorry in advance).
+
+# tysocDart: A locomotion toolkit, with Dart as physics backend.
+
+This is an instance of my [**tysoc**](https://github.com/wpumacay/tysocCore) framework for locomotion, 
+using [**DART**](http://dartsim.github.io/) as physics backend. As explained in the core repository, the idea
+is to provide a kind of abstraction layer on top of various physics backends, and allow you to just
+focus on making your experiment regardless of the details of each specific backend.
+
+I will be adding more documentation as I develop the library, and sorry in advance as I might forget 
+to update the docs from time to time. However, one main objective is to write comprehensive documentation, 
+and I will be doing it on the go. If you have any suggestions/issues, just post an issue or contact me 
+at wpumacay@gmail.com .
--- scripts/setup_dependencies.sh
+++ scripts/setup_dependencies.sh
@@ -1,45 +1,24 @@
-#!/bin/sh
-
-# --------------------------------------------------------------------- #
-# script/setup: Sets up this repo by cloning all required dependencies  #
-# --------------------------------------------------------------------- #
-
-echo "======== Installing apt-dependencies ============"
-
-echo "==> Installing build tools"
-sudo apt-get install build-essential cmake pkg-config
-
-echo "==> Installing dependencies for renderer"
-sudo apt-get install libassimp-dev libglfw3-dev libglew-dev
+#!/usr/bin/env bash
 
 echo "==> Installing dependencies for dart-backend"
 sudo apt-get install libeigen3-dev libassimp-dev libccd-dev libfcl-dev libboost-regex-dev libboost-system-dev libopenscenegraph-dev
 
-echo "======== Cloning dependencies -> ext/ ==========="
+GIT_DEPS_REPO=(tiny_math pybind11 imgui spdlog tiny_renderer tysoc dart)
+GIT_DEPS_USER=(wpumacay pybind wpumacay gabime wpumacay wpumacay wpumacay)
+GIT_DEPS_BRANCH=(tysoc-stable master docking v1.x tysoc-stable master master)
+GIT_DEPS_DEST=(ext/tiny_math ext/pybind11 ext/imgui ext/spdlog ext/tiny_renderer core ext/dart)
 
-# rendering engine for visualizer
-echo "==> Cloning wpumacay/cat1 @ github - master branch"
-git clone https://github.com/wpumacay/cat1.git ext/cat1
-
-# using own imgui version to add some extra cmake-files
-echo "==> Cloning wpumacay/imgui @ github - master branch"
-git clone --branch=docking https://github.com/wpumacay/imgui.git ext/imgui
-
-# spdlog library (for our logging system)
-echo "==> Cloning gabime/spdlog @ github - master branch"
-git clone https://github.com/gabime/spdlog.git ext/spdlog
-
-# using own dart version without all the assets and stuff
-echo "==> Cloning wpumacay/dart @ github - master branch"
-git clone https://github.com/wpumacay/dart.git ext/dart
-
-# used for generating pytysoc|python bindings
-echo "==> Cloning pybind/pybind11 @ github - master branch"
-git clone https://github.com/pybind/pybind11.git ext/pybind11
-
-echo "========== Cloning core library -> core ========="
-
-# core interface of this library (recall this repo is an extension with support for dart)
-echo "==> Cloning wpumacay/tysoc @ github - master branch"
-git clone https://github.com/wpumacay/tysoc.git core
-
+for i in {0..6}
+do
+    USER=${GIT_DEPS_USER[$i]}
+    REPO=${GIT_DEPS_REPO[$i]}
+    BRANCH=${GIT_DEPS_BRANCH[$i]}
+    URL=https://github.com/${USER}/${REPO}
+    if [ ! -d "${GIT_DEPS_DEST[$i]}" ]
+    then
+        echo "===> Cloning ${USER}/${REPO} @ github - ${BRANCH} branch"
+        git clone --branch=${BRANCH} ${URL} ${GIT_DEPS_DEST[$i]}
+    else
+        echo "===> Repository ${USER}/${REPO} @ github already checked out"
+    fi
+done
